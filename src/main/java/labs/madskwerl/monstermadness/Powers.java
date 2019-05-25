a104d1189acf15ffb45a62db1942421f35220628
MadSkwerl <fruitloops.johnson@gmail.com>
2019-05-24 20:02:09 -0400

    Update Powers.java

--- src/main/java/labs/madskwerl/monstermadness/Powers.java
+++ src/main/java/labs/madskwerl/monstermadness/Powers.java
@@ -54,8 +54,8 @@ public class Powers
             case "ROBBING":     //durability decreases with time
             case "AMMO REGEN":  //durability increases with time
                 return 1;
-            case "CHARITY":     //gives life to enemy on hit (precentage most likley)
-            case "VAMP":        //takes life from enemey on hit (precentage?)
+            case "CHARITY":     //gives life to enemy on hit (percentage most likely)
+            case "VAMP":        //takes life from enemy on hit (percentage?)
                 return 2;
             case "DYING":       //life drains over time
             case "YOUTH":       //life regens over time
@@ -69,7 +69,7 @@ public class Powers
             case "TAINTED":     //chance to be poisoned on hit
             case "TOXIC":       //poisons on hit
                 return 6;
-            case "DRUGGED":     //increases damage from being poisioned
+            case "DRUGGED":     //increases damage from being poisoned
             case "ANTIDOTE":    //protects/reduces being poisoned
                 return 7;
             case "VOLATILE":    //chance to explode when using (damage dealt may be unhealable?)
@@ -81,7 +81,7 @@ public class Powers
             case "QUICKEN":     //speeds enemies movement on hit
             case "TRIPPY":      //slows enemies movement on hit
                 return 10;
-            case "DEADWEIGHT":  //increases
+            case "SHACKLE":  //increases slowing of player
             case "OIL":         //reduces slowing of the player
                 return 11;
             case "TURTLE":      //slows player movement speed
@@ -93,16 +93,47 @@ public class Powers
             case "DRY":         //increases fire damage taken
             case "MOIST":       //decreases fire damage taken
                 return 14;
-            case "INSTAGIB":   //increases chnace to insta kill
+            case "INSTAGIB":    //increases chance to insta kill
                 return 15;
-            case "INSTA PRUF": //decreases insta kill damage 1 = 50% hp 2 = immune
+            case "INSTA PRUF":  //decreases insta kill damage 1 = 50% hp 2 = immune
                 return 16;
-            case "HALTING":
-            case "CONFOUNDING":
+            case "HALTING":     //chance to be stunned on hit
+            case "CONFOUNDING": //chance (smaller that other power ups) to stun on hit
                 return 17;
-
-
-
+            case "DAZZELMENT":  //either increases time player is stunned or creates chance to randomly be stunned over time
+            case "UNLEASHED":   //level 0 = 100% stun chance lvl1 = 50% lvl2 = 0% (immune). only applies to mobs that hit with stunning
+                return 18;
+            case "TIRING":      //Slows atk speed
+            case "FLURRY":      //speeds atk speed
+                return 19;
+            case "FATIGUE":     //increases the amount of atk speed reduction when taking dam from mob with atk spd reduction
+            case "ENERGY":      //decreases " ". lvl0=0% lvl1=50% lvl2 = immune
+                return 20;
+            case "SHORTY":     //decreases jumping distance (maybe speed a little
+            case "SPRING":     //increases jumping distance
+                return 21;
+            case "AERODYNAMIC":  //increases fall damage
+            case "FEATHER":    //decreases fall damage
+                return 22;
+            case "ANGERING":   //chance to greatly increase speed, atk speed and take aggro of mob
+            case "CHARMING":   //chance to pacify mob
+                return 23;
+            case "NOISY":       //increases the chance to be detected by a mob
+            case "STEALTH":      //increases invisibility level
+                return 24;
+            case "UNSTABLE":     //increases the amount an enemy pushes the player on hit
+            case "PUSH":        //increases the amount an enemy is knocked back
+                return 25;
+            case "LEANING":    //increases the amount an enemy pushes the player on hit (yeah im lazy)
+            case "PULL":        //increases the amount an enemy is drawn in on hit. will cancel out push 1:1 = 0
+                return 26;
+            case "BALANCE":     //increases the amount of knock around from all sources and player experiences
+            case "STURDY":      //decreases the amount a player can be pushed or pulled on hit. decreases explosive knock back
+                return 27;
+            case "FRAGILE":    //increases the amount on durability uses per hit
+            case "SPLASH":     //increases splash range on hit pos only
+                return 28;
+            case "RETENTION": //lvl1 = weapon is saved it death & equipped. lvl2 weapon is always saved
             default:
                 return -2;
 
