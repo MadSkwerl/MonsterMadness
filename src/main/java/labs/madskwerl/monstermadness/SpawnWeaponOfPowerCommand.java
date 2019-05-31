0fe70da8f9718dddff3199281ec167e451acb861
kelifire <mom005@mymail.tc3.edu>
2019-05-30 21:46:07 -0400

    Add Remove
    Add remove command

--- src/main/java/labs/madskwerl/monstermadness/SpawnWeaponOfPowerCommand.java
+++ src/main/java/labs/madskwerl/monstermadness/SpawnWeaponOfPowerCommand.java
@@ -6,7 +6,9 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class SpawnWeaponOfPowerCommand implements CommandExecutor {
@@ -24,55 +26,97 @@ public class SpawnWeaponOfPowerCommand implements CommandExecutor {
         int powerID = 8;
         if (sender instanceof Player)
         {
+
+
+            System.out.println("Sender: " + ((Player) sender).getDisplayName());
+            System.out.println("Command: " + command.toString());
+            System.out.println("label: " + label);
+            System.out.println("args: " + Arrays.deepToString(args));
             player = (Player) sender;
 
+            //====================================Start Block: No Args==========================================
+            if (args.length == 0)
+            {
           /*  ItemStack itemStack = new ItemStack(Material.IRON_SWORD,1 );
             wopVault.newWOP(itemStack, powerID,(int)(random.nextDouble() * 21) -10);
             player.getInventory().addItem(itemStack);
             */
-            ItemStack itemStack = new ItemStack(Material.IRON_SWORD,1 );
-            wopVault.newWOP(itemStack, powerID,-10);
-            player.getInventory().addItem(itemStack);
+                ItemStack itemStack = new ItemStack(Material.IRON_SWORD, 1);
+                wopVault.newWOP(itemStack, powerID, -10);
+                player.getInventory().addItem(itemStack);
 
-            itemStack = new ItemStack(Material.IRON_SWORD,1 );
-            wopVault.newWOP(itemStack, powerID,-8);
-            player.getInventory().addItem(itemStack);
+                itemStack = new ItemStack(Material.IRON_SWORD, 1);
+                wopVault.newWOP(itemStack, powerID, -8);
+                player.getInventory().addItem(itemStack);
 
-             itemStack = new ItemStack(Material.IRON_SWORD,1 );
-            wopVault.newWOP(itemStack, powerID,-6);
-            player.getInventory().addItem(itemStack);
+                itemStack = new ItemStack(Material.IRON_SWORD, 1);
+                wopVault.newWOP(itemStack, powerID, -6);
+                player.getInventory().addItem(itemStack);
 
-            itemStack = new ItemStack(Material.IRON_SWORD,1 );
-            wopVault.newWOP(itemStack, powerID,-4);
-            player.getInventory().addItem(itemStack);
+                itemStack = new ItemStack(Material.IRON_SWORD, 1);
+                wopVault.newWOP(itemStack, powerID, -4);
+                player.getInventory().addItem(itemStack);
 
-            itemStack = new ItemStack(Material.IRON_SWORD,1 );
-            wopVault.newWOP(itemStack, powerID,-2);
-            player.getInventory().addItem(itemStack);
+                itemStack = new ItemStack(Material.IRON_SWORD, 1);
+                wopVault.newWOP(itemStack, powerID, -2);
+                player.getInventory().addItem(itemStack);
 
-            itemStack = new ItemStack(Material.IRON_SWORD,1 );
-            wopVault.newWOP(itemStack, powerID,0);
-            player.getInventory().addItem(itemStack);
+                itemStack = new ItemStack(Material.IRON_SWORD, 1);
+                wopVault.newWOP(itemStack, powerID, 0);
+                player.getInventory().addItem(itemStack);
 
-            itemStack = new ItemStack(Material.IRON_SWORD,1 );
-            wopVault.newWOP(itemStack, powerID,2);
-            player.getInventory().addItem(itemStack);
+                itemStack = new ItemStack(Material.IRON_SWORD, 1);
+                wopVault.newWOP(itemStack, powerID, 2);
+                player.getInventory().addItem(itemStack);
 
-            itemStack = new ItemStack(Material.IRON_SWORD,1 );
-            wopVault.newWOP(itemStack, powerID,4);
-            player.getInventory().addItem(itemStack);
+                itemStack = new ItemStack(Material.IRON_SWORD, 1);
+                wopVault.newWOP(itemStack, powerID, 4);
+                player.getInventory().addItem(itemStack);
 
-            itemStack = new ItemStack(Material.IRON_SWORD,1 );
-            wopVault.newWOP(itemStack, powerID,6);
-            player.getInventory().addItem(itemStack);
+                itemStack = new ItemStack(Material.IRON_SWORD, 1);
+                wopVault.newWOP(itemStack, powerID, 6);
+                player.getInventory().addItem(itemStack);
 
-            itemStack = new ItemStack(Material.IRON_SWORD,1 );
-            wopVault.newWOP(itemStack, powerID,8);
-            player.getInventory().addItem(itemStack);
+                itemStack = new ItemStack(Material.IRON_SWORD, 1);
+                wopVault.newWOP(itemStack, powerID, 8);
+                player.getInventory().addItem(itemStack);
 
-            itemStack = new ItemStack(Material.IRON_SWORD,1 );
-            wopVault.newWOP(itemStack, powerID,10);
-            player.getInventory().addItem(itemStack);
+                itemStack = new ItemStack(Material.IRON_SWORD, 1);
+                wopVault.newWOP(itemStack, powerID, 10);
+                player.getInventory().addItem(itemStack);
+            }//=======================================End Block: No Args=========================================
+            //========================================Start Block: Remove========================================
+            else if (args[0].toLowerCase().equals("remove") ||
+                    args[0].toLowerCase().equals("rm"))
+            {
+                if (args.length > 1)
+                {
+                    if (args[1].toLowerCase().equals("h") ||
+                        args[1].toLowerCase().equals("hand"))
+                    {
+                        ItemStack item = player.getInventory().getItemInMainHand();
+                        if (item != null && item.hasItemMeta() &&
+                            item.getItemMeta().hasLocalizedName() &&
+                            item.getItemMeta().getLocalizedName().toLowerCase().contains("wop")) //Remove item in hand/cursor if it is a WOP
+                        {
+                            item.setAmount(0);
+                        }
+                    }
+                }else //Remove all weapons of power in player's inventory
+                {
+                    for (ItemStack item : player.getInventory().getContents())
+                    {
+                        if (item != null && item.hasItemMeta() &&
+                            item.getItemMeta().hasLocalizedName() &&
+                            item.getItemMeta().getLocalizedName().toLowerCase().contains("wop"))
+                        {
+                            item.setAmount(0);
+                        }
+                    }
+                }
+                player.updateInventory();
+            }
+            //=====================================End Block: Remove=============================================
         }
         else
             return false;
